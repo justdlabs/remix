ef683d319b59a135f584fd112a341280405d629e
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-10-14 12:52:52 +0700

    lfg

--- app/components/ui/text-field.tsx
+++ app/components/ui/text-field.tsx
@@ -1,8 +1,8 @@
-
+'use client'
 
 import * as React from 'react'
 
-import { TextInputDOMProps } from '@react-types/shared'
+import type { TextInputDOMProps } from '@react-types/shared'
 import { IconEye, IconEyeClosed, IconLoader } from 'justd-icons'
 import {
   Button as ButtonPrimitive,
